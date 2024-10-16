84f7a4a9779ea5f89de4f0301bf3d329ba1a2619
Shyam Ramachandran <janshyam03@tamu.edu>
2024-10-16 12:18:02 -0500

    Create helper.h

---
+++ MPI_Builds/helper.h
@@ -0,0 +1,11 @@
+// helper.h
+#ifndef HELPER_H
+#define HELPER_H
+
+int* sortedArray(int n);
+int* randomArray(int n);
+int* reverseArray(int n);
+int* perturbedArray(int n);
+void deleteArray(int* a);
+
+#endif
